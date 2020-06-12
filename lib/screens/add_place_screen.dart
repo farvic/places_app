7640468973a46685ef514bcf33c07207ff346c88
Victor <victorfa@dcomp.ufs.br>
2020-06-12 02:16:06 -0300

    The app can access google maps

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -2,9 +2,11 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 import '../providers/great_places.dart';
 
 import '../widgets/image_input.dart';
+import '../widgets/location_input.dart';
 
 class AddPlaceScreen extends StatefulWidget {
   static const routeName = '/add-place';
@@ -50,6 +52,8 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                     ),
                     SizedBox(height:10),
                     ImageInput(_selectImage),
+                    SizedBox(height:10),
+                    LocationInput(),
                   ],
                 ),
               ),
